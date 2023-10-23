6de22c61fa95aeadb65e77038be53c6d3bda41ba
Rebecca-L28 <lee.rebecca1@ufl.edu>
2023-10-23 10:41:04 -0400

    encoder function

--- main.py
+++ main.py
@@ -1,10 +1,14 @@
 # Rebecca Lee's encoder function
-def encoder():
-    password = input("Enter a password to be encoded: ")
+def encoder(password):
     encoded_password = ""
     for num in password:
         new_num = (int(num) + 3) % 10
         encoded_password += str(new_num)
-    print(f"Encoded password: {encoded_password}")
+    return encoded_password
 
-    encoder()
+def main():
+    password = input("Enter a password to be encoded: ")
+    print(f"Encoded passwaord: {encoder(password)}")
+
+if __name__ == '__main__':
+    main()
