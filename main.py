0da21cded43950945d8b307a7355f11ea6706ab1
Rebecca-L28 <lee.rebecca1@ufl.edu>
2023-10-23 15:20:08 -0400

    Didn't see the sample output part, didn't realize there was supposed to be a menu.

--- main.py
+++ main.py
@@ -7,10 +7,23 @@ def encoder(password):
     return encoded_password
 
 def main():
-    password = input("Enter a password to be encoded: ")
-    print(f"Encoded passwaord: {encoder(password)}")
-    encoded_password = input("Enter a password to be decoded: ")
-    print(f"Decoded password: {decode(encoded_password)}")
+    while True:
+        print("Menu")
+        print("-------------")
+        print("1. Encode")
+        print("2. Decode")
+        print("3. Quit")
+        menu_choice = input("Please enter an option: ")
+
+        if menu_choice == "1":
+            password = input("Please enter your password to encode: ")
+            password = encoder(password)
+            print("Your password has been encoded and stored!")
+        if menu_choice == "2":
+            encoded_password = decode(password)
+            print(f"The encoded password is {password}, and the original password is {encoded_password}")
+        if menu_choice == "3":
+            break
 
 def decode(str_num):
     decoded = ""
